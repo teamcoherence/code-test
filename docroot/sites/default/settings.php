c21335a59f323340252359db3bceed04d0a6f314
Xavier Mirabelli-Montan <xavier@coherence.co.uk>
2021-03-15 21:53:57 +0000

    Removing shortcut config to stop issues

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -779,6 +779,11 @@ $settings['migrate_node_migrate_type_classic'] = FALSE;
 # if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
 #   include $app_root . '/' . $site_path . '/settings.local.php';
 # }
+
+$settings['config_exclude_modules'] = [
+  'shortcut'
+];
+
 $databases['default']['default'] = array (
   'database' => 'drupal9',
   'username' => 'drupal9',
